aa17b37395ee2a15aa013fa522ae9543fc5a0158
xypine <elias.eskelinen@pm.me>
2024-12-07 23:57:34 +0000

    Deploying to gh-pages from @ xypine/nlcep@2416f36a52237e6fbd6fd5e71db1e12c5cfad7e5 🚀

---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nlcep",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"nlcep/enum.EventParseError.html\" title=\"enum nlcep::EventParseError\">EventParseError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[274]}
